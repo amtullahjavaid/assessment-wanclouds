3e9318627802cd2267dba3813359f5b0fd4b1092
amtullahjavaid <amtullahjavaid11@gmail.com>
2025-04-29 20:16:44 +0500

    Implement persistent Storage

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1. Set up the initial React app using create-react-app
 2. Created the folder structure following best practices
 3. Implemented Redux store for state management
-4. Created layout components (Header, Sidebar, Footer)
+4. Created layout components (Header, Sidebar, Content, Footer)
 5. Implemented routing with React Router
 6. Created form with validation using Formik and Yup
 7. Implemented table view with delete functionality
@@ -17,3 +17,7 @@
 
 ## Issues Faced and Solutions
 I did not experience any issues in the implementation of this task.
+
+## Additional Features
+1. Implemented persistent storage.
+2. Implemented light and dark theme.
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,56 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import userReducer from './userSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     users: userReducer
+//   }
+// });
+
+// export default store;
+
+// src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+
+// Load state from localStorage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('userState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error('Could not load state', err);
+    return undefined;
+  }
+};
+
+// Save state to localStorage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('userState', serializedState);
+  } catch (err) {
+    console.error('Could not save state', err);
+  }
+};
+
+const persistedState = loadState();
 
 const store = configureStore({
   reducer: {
     users: userReducer
-  }
+  },
+  preloadedState: persistedState
+});
+
+// Subscribe to store changes and save to localStorage
+store.subscribe(() => {
+  saveState({
+    users: store.getState().users
+  });
 });
 
 export default store;
