e89932c563bebf6d67a58d3faa449f900672cf19
amtullahjavaid <amtullahjavaid11@gmail.com>
2025-04-29 11:36:15 +0500

    User Data Form

---
+++ src/pages/FormPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserForm from '../components/UserForm/UserForm';
+
+const FormPage = () => {
+  return (
+    <div className="page form-page">
+      <UserForm />
+    </div>
+  );
+};
+
+export default FormPage;
