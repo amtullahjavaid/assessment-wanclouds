5ab01a818a93577771121b9aff07e67d0ade4564
amtullahjavaid <amtullahjavaid11@gmail.com>
2025-04-29 22:39:08 +0500

    Minor Changes
    Changed the header component to integrate better with additional features

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,25 @@
+// import React from 'react';
+
+// const Header = () => {
+//   return (
+//     <header className="header">
+//       <h1>User Management System</h1>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+// src/components/Layout/Header.js
 import React from 'react';
+import ThemeToggle from '../ThemeToggle/ThemeToggle';
 
 const Header = () => {
   return (
     <header className="header">
       <h1>User Management System</h1>
+      <ThemeToggle />
     </header>
   );
 };
