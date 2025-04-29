8d14677b77d4d3a94a6b8d2f3cff687bcd3ed72f
amtullahjavaid <amtullahjavaid11@gmail.com>
2025-04-29 13:22:16 +0500

    Add Form Page and Table Page

---
+++ src/pages/TablePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserTable from '../components/UserTable/UserTable';
+
+const TablePage = () => {
+  return (
+    <div className="page table-page">
+      <UserTable />
+    </div>
+  );
+};
+
+export default TablePage;
